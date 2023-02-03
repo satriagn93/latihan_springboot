4f57f830f4071475f26342a3031193446b3d7e40
satria girinanda <satriagn93@gmail.com>
2023-02-03 10:54:48 +0700

    first commit

---
+++ src/main/java/com/spring1/Springapi1Application.java
@@ -0,0 +1,13 @@
+package com.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springapi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring1/springapi1/Springapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring1.springapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
